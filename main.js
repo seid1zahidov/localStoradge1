a81134008f1b81445d1aeb2653524cbed2bab670
seid1zahidov <seid.zahidov15@gmail.com>
2025-06-02 16:44:31 +0400

    lc1

---
+++ main.js
@@ -0,0 +1,79 @@
+// const div = document.querySelector(".div")
+// const changeBtn  = document.querySelector(".ChangeBtn")
+
+// let mode = localStorage.getItem("mode") || "white";
+
+// div.classList.add(mode)
+
+// changeBtn.addEventListener("click" , () => {
+//     if(div.classList.contains("dark")){
+//         div.classList.replace("dark" , "white")
+//         localStorage.setItem("mode" , "white")
+//     }else{
+//         div.classList.replace("white","dark")
+//         localStorage.setItem("mode" , "dark")
+//     }
+// })
+
+
+
+
+// const slides = document.querySelectorAll('.slide');
+// const prev = document.getElementById('prev');
+// const next = document.getElementById('next');
+
+// let index = 0;
+
+// function showSlide(i) {
+//   slides.forEach(slide => slide.classList.remove('active'));
+//   slides[i].classList.add('active');
+// }
+
+// prev.addEventListener('click', () => {
+//   index = (index - 1 + slides.length) % slides.length;
+//   showSlide(index);
+// });
+
+// next.addEventListener('click', () => {
+//   index = (index + 1) % slides.length;
+//   showSlide(index);
+// });
+
+
+
+const slides = document.querySelectorAll('.slide');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+let current = 0;
+const now = new Date().getTime();
+const threeDays = 3 * 24 * 60 * 60 * 1000;
+function setSliderData() {
+  localStorage.setItem("sliderShown", "yes");
+  localStorage.setItem("sliderExpiry", (now + threeDays).toString());
+}
+function checkSliderExpiry() {
+  const expiry = localStorage.getItem("sliderExpiry");
+  if (!expiry || new Date().getTime() > Number(expiry)) {
+    localStorage.removeItem("sliderShown");
+    localStorage.removeItem("sliderExpiry");
+    console.log("Slider müddəti bitib, məlumat silindi.");
+  }
+}
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[index].classList.add('active');
+}
+
+prev.addEventListener('click', () => {
+  current = (current - 1 + slides.length) % slides.length;
+  showSlide(current);
+});
+next.addEventListener('click', () => {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+});
+checkSliderExpiry(); 
+
+!localStorage.getItem("sliderShown") ? setSliderData(): console.log(" ") 
+showSlide(current);
+
